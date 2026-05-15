12fd3b396904cbf7a3a9d44b671cc0ceb599438b
Guilherme Portella <guilhermeportella2@gmail.com>
2026-05-14 23:45:57 -0300

    feat: update stylesheet link version and remove text decoration from links across the site

--- internal/transport/http/router_test.go
+++ internal/transport/http/router_test.go
@@ -44,7 +44,7 @@ func TestNewRouterHome(t *testing.T) {
 		}
 	}
 
-	if !strings.Contains(body, `<link rel="stylesheet" href="/static/css/main.css?v=20260512-404">`) {
+	if !strings.Contains(body, `<link rel="stylesheet" href="/static/css/main.css?v=20260515-links">`) {
 		t.Fatalf("body does not contain stylesheet")
 	}
 
